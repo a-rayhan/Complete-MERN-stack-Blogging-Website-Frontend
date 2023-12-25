43c2ce710ab35ee44e47f85f489a91962b2db038
rayhan <aburayhandh@gmail.com>
2023-12-25 17:02:30 +0600

    Use state for search bar for visibity or not visibility on mobile devices

--- src/components/navbar.component.jsx
+++ src/components/navbar.component.jsx
@@ -1,7 +1,11 @@
 import { Link } from 'react-router-dom';
 import logo from '../imgs/logo.png';
+import { useState } from 'react';
 
 const Navbar = () => {
+
+    const [searchBoxVisibility, setSearchBoxVisibility] = useState(false)
+
     return (
         <nav className="navbar">
 
@@ -9,7 +13,7 @@ const Navbar = () => {
                 <img src={logo} alt="" className='w-full' />
             </Link>
 
-            <div className='absolute bg-white w-full left-0 top-full mt-0.5 border-b border-grey py-4 px-[5vw] md:border-0 md:block md:relative md:inset-0 md:p-0 md:w-auto'>
+            <div className={"absolute bg-white w-full left-0 top-full mt-0.5 border-b border-grey py-4 px-[5vw] md:border-0 md:block md:relative md:inset-0 md:p-0 md:w-auto md:show " + (searchBoxVisibility ? "show" : "hide")}>
                 <input
                     type="text"
                     placeholder='Search'
@@ -20,7 +24,10 @@ const Navbar = () => {
             </div>
 
             <div className='flex items-center gap-3 md:gap-6 ml-auto'>
-                <button className='md:hidden bg-grey w-12 h-12 rounded-full flex items-center justify-center'>
+                <button
+                    onClick={() => setSearchBoxVisibility(currenVal => !currenVal)}
+                    className='md:hidden bg-grey w-12 h-12 rounded-full flex items-center justify-center'
+                >
                     <i className="fi fi-rr-search text-xl"></i>
                 </button>
             </div>
